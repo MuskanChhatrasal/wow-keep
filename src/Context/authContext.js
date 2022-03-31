35fbe8d5599d5275718fdff04e8703dbb5902f2a
Muskan <muskanchhatrasal@gmail.com>
2022-03-31 18:35:00 +0530

    feat: Signup functionality added

---
+++ src/Context/authContext.js
@@ -0,0 +1,72 @@
+import { createContext, useContext, useReducer } from "react";
+import axios from "axios";
+// import {authReducer} from './authReducer'
+
+const initialAuthData = {
+    toastData:{
+        display: false,
+        data: "",
+        status:""
+    },
+    signedIn: false,
+    userName: "",
+    userEmail: "",
+    userID: "",
+    notes: [],
+    archivedNotes: [],
+    trashNotes: []
+}
+
+const authReducer = (state, { type, payload }) => {
+	switch (type) {
+		case "LOGIN SUCCESS":
+			return { ...state, toastData: { display: true, data: payload.toastMessage, status: "success" }, signedIn: true, userName: payload.name, userEmail: payload.email, userID: payload.id }
+		case "LOGIN_ERROR":
+			return { ...state, toastData: { display: true, data: payload.toastMessage, status: "alert" } }
+
+		case "HANDLER_ERROR":
+			return { ...state, toastData: { display: true, data: payload.toastMessage, status: "alert" } }
+
+		case "REMOVE_TOAST":
+			return { ...state, toastData: { ...state.toastData, display: false } }
+
+		case "LOGOUT":
+			return { ...state, toastData: { display: true, data: payload.toastMessage, status: "alert" }, signedIn: false, userName: payload.name, userEmail: payload.email, userID: payload.id }
+		default:
+			return { ...state }
+	}
+}
+
+const AuthContext = createContext(initialAuthData );
+
+const AuthProvider = ({children}) =>{
+
+    const [authState, authDispatch] = useReducer(authReducer, initialAuthData)
+
+    const signUp = async(userDetails) => {
+            console.log(userDetails)
+        try{
+            const response = await axios.post("/api/auth/signup", {
+                firstName: userDetails.firstName,
+                lastName: userDetails.lastName,
+                email: userDetails.email,
+                password: userDetails.password
+            })
+            
+                localStorage.setItem("tokenNotesApp", response.data.encodedToken);
+                authDispatch({type: "LOGIN_SUCCESS", payload: {toastMessage: "Signed up", name: response.data.createdUser.firstName, email: response.data.createdUser.email, id:response.data.createdUser._id }})
+                  
+        }catch(error){
+            console.log(error);
+            authDispatch({type:"HANDLER_ERROR", payload:{toastMessage: "Handler error"}})
+        }
+
+    }
+    return (
+        <AuthContext.Provider value={{authState, authDispatch, signUp}}>{children}</AuthContext.Provider>
+    )
+}
+
+const useAuth = () => useContext(AuthContext);
+
+export {useAuth, AuthProvider}
